0320d052c71fc03ffb306a302e1790502b9e4ea7
berzz26 <aumtamboli2233@gmail.com>
2024-11-09 01:28:39 +0530

    initialised v1

--- src/constants.h
+++ src/constants.h
@@ -1,2 +1,15 @@
-#define WINDOW_WIDTH 640;
-#define WINDOW_HEIGHT 400;
+#define WINDOW_WIDTH  480
+
+#define WINDOW_HEIGHT  480
+
+#define PIPE_WIDTH  80
+
+#define PIPE_GAP  150
+
+#define BIRD_SIZE  20
+
+#define GRAVITY  1
+
+#define JUMP_STRENGTH  -10
+
+#define PIPE_SPEED  3
--- src/main.c
+++ src/main.c
@@ -1,109 +1,166 @@
 #include <SDL2/SDL.h>
+#include <stdbool.h>
 #include <stdio.h>
-#include "./constants.h"
+#include <stdlib.h>
+#include <time.h>
+#include"./constants.h"
+// Window and game constants
 
 
+// Bird structure
+typedef struct {
+    float x, y;        // Position of the bird
+    float velocity;    // Current velocity (used for gravity and jump)
+} Bird;
 
+// Pipe structure
+typedef struct {
+    int x;             // Horizontal position of the pipe
+    int height;        // Height of the top pipe
+} Pipe;
 
-//global 
-SDL_Window *window = NULL;
-SDL_Renderer *renderer = NULL;
-int game_is_running = 0;
-
-int window_initialize(void)
-{
-
-    if (SDL_Init(SDL_INIT_EVERYTHING) != 0)
-    {
-        fprintf(stderr, "cant run SDL\n");
-        return 0;
+// Function to initialize SDL components
+bool initSDL(SDL_Window **window, SDL_Renderer **renderer) {
+    // Initialize SDL with video capabilities
+    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+        printf("SDL could not initialize! SDL Error: %s\n", SDL_GetError());
+        return false;
     }
 
-    window = SDL_CreateWindow(
-        NULL,
-        SDL_WINDOWPOS_CENTERED,
-        SDL_WINDOWPOS_CENTERED,
-        640,
-        400,
-        SDL_WINDOW_BORDERLESS
-
-    );
-
-    if (!window)
-    {
-        fprintf(stderr, "Error genrating window\n");
-        return 0;
+    // Create the game window
+    *window = SDL_CreateWindow("Flappy Bird", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, WINDOW_WIDTH, WINDOW_HEIGHT, SDL_WINDOW_SHOWN);
+    if (*window == NULL) {
+        printf("Window could not be created! SDL Error: %s\n", SDL_GetError());
+        return false;
     }
 
-    renderer = SDL_CreateRenderer(
-        window,
-        -1,
-        0);
-
-    if (!renderer)
-    {
-        fprintf(stderr, "error rendering window\n");
-        return 0;
+    // Create a renderer for the window (this is where graphics are drawn)
+    *renderer = SDL_CreateRenderer(*window, -1, SDL_RENDERER_ACCELERATED);
+    if (*renderer == NULL) {
+        printf("Renderer could not be created! SDL Error: %s\n", SDL_GetError());
+        return false;
     }
-
-    printf("window created without errors\n");
-
-    return 1;
+    return true;
 }
 
-void process_input()
-{
+// Function to handle user input for jumping and quitting
+void handleInput(bool *isRunning, Bird *bird) {
     SDL_Event event;
-    SDL_PollEvent(&event);
-
-    switch (event.type)
-    {
-    case SDL_QUIT:
-        game_is_running = 0;
-        break;
-
-    case SDL_KEYDOWN:
-        if(event.key.keysym.sym == SDLK_ESCAPE){
-            game_is_running = 0;
-            break;
+    while (SDL_PollEvent(&event) != 0) { // Process all events in the event queue
+        if (event.type == SDL_QUIT) {     // If the user closes the window
+            *isRunning = false;
         }
-    
-    default:
-        break;
+        if (event.type == SDL_KEYDOWN && event.key.keysym.sym == SDLK_SPACE) {
+            // If the space key is pressed, make the bird jump by adjusting velocity
+            (*bird).velocity = JUMP_STRENGTH;
+        }
+    }
+}
+
+// Function to update bird position and apply gravity
+void updateBird(Bird *bird) {
+    (*bird).velocity += GRAVITY; // Increase downward velocity due to gravity
+    (*bird).y += (*bird).velocity; // Update bird's y-position with new velocity
+
+    // Prevent bird from moving off the top of the screen
+    if ((*bird).y < 0) {
+        (*bird).y = 0;
+        (*bird).velocity = 0;
     }
 
+    // Prevent bird from falling below the bottom of the screen
+    if ((*bird).y + BIRD_SIZE > WINDOW_HEIGHT) {
+        (*bird).y = WINDOW_HEIGHT - BIRD_SIZE;
+        (*bird).velocity = 0;
+    }
 }
 
-void setup(){
-    //todo
-}
-void update()
-{
-    //todo
-}
-void render()
-{
-    //todo
+// Function to update pipe position and reset if it goes off-screen
+void updatePipe(Pipe *pipe) {
+    (*pipe).x -= PIPE_SPEED; // Move pipe to the left
+
+    // Reset pipe to the right side of the screen if it goes off-screen
+    if ((*pipe).x < -PIPE_WIDTH) {
+        (*pipe).x = WINDOW_WIDTH; // Move it back to the right edge
+        (*pipe).height = rand() % (WINDOW_HEIGHT - PIPE_GAP); // Randomize pipe height
+    }
 }
 
-void destroy_window()
-{
+// Function to render the bird, pipes, and background
+void render(SDL_Renderer *renderer, Bird bird, Pipe pipe) {
+    // Set background color to sky blue and clear the screen
+    SDL_SetRenderDrawColor(renderer, 135, 206, 235, 255);
+    SDL_RenderClear(renderer);
+
+    // Draw the bird as a yellow square
+    SDL_Rect birdRect = { (int)bird.x, (int)bird.y, BIRD_SIZE, BIRD_SIZE };
+    SDL_SetRenderDrawColor(renderer, 255, 255, 0, 255); // Yellow color
+    SDL_RenderFillRect(renderer, &birdRect);
+
+    // Draw the top and bottom pipes as green rectangles
+    SDL_Rect pipeRectTop = { pipe.x, 0, PIPE_WIDTH, pipe.height };
+    SDL_Rect pipeRectBottom = { pipe.x, pipe.height + PIPE_GAP, PIPE_WIDTH, WINDOW_HEIGHT - pipe.height - PIPE_GAP };
+    SDL_SetRenderDrawColor(renderer, 34, 139, 34, 255); // Green color
+    SDL_RenderFillRect(renderer, &pipeRectTop);
+    SDL_RenderFillRect(renderer, &pipeRectBottom);
+
+    // Display the rendered content on the screen
+    SDL_RenderPresent(renderer);
+}
+
+// Function to check for collision with pipes or ground
+bool checkCollision(Bird bird, Pipe pipe) {
+    // Check if bird hits top or bottom of the screen
+    if (bird.y < 0 || bird.y + BIRD_SIZE > WINDOW_HEIGHT) {
+        return true;
+    }
+
+    // Check if bird hits a pipe
+    if (bird.x + BIRD_SIZE > pipe.x && bird.x < pipe.x + PIPE_WIDTH) {
+        if (bird.y < pipe.height || bird.y + BIRD_SIZE > pipe.height + PIPE_GAP) {
+            return true;
+        }
+    }
+    return false;
+}
+
+// Main game loop
+int main(int argc, char *args[]) {
+    srand(time(NULL)); // Seed random number generator for pipe height
+
+    SDL_Window *window = NULL;
+    SDL_Renderer *renderer = NULL;
+    if (!initSDL(&window, &renderer)) { // Initialize SDL components
+        return 1;
+    }
+
+    // Initialize bird and pipe objects
+    Bird bird = { .x = WINDOW_WIDTH / 4, .y = WINDOW_HEIGHT / 2, .velocity = 0 };
+    Pipe pipe = { .x = WINDOW_WIDTH, .height = rand() % (WINDOW_HEIGHT - PIPE_GAP) };
+
+    bool isRunning = true; // Game running flag
+    while (isRunning) {
+        handleInput(&isRunning, &bird); // Handle user input
+        updateBird(&bird); // Update bird position and apply gravity
+        updatePipe(&pipe); // Move pipe to the left and reset if off-screen
+        
+        // Check for collision and reset if bird collides
+        if (checkCollision(bird, pipe)) {
+            printf("Game Over!\n");
+            SDL_Delay(1000); // Pause for a second before resetting
+            bird.y = WINDOW_HEIGHT / 2; // Reset bird position
+            bird.velocity = 0; // Reset bird velocity
+            pipe.x = WINDOW_WIDTH; // Reset pipe position
+            pipe.height = rand() % (WINDOW_HEIGHT - PIPE_GAP); // New random height for pipe
+        }
+        
+        render(renderer, bird, pipe); // Draw the game elements
+        SDL_Delay(16); // Delay to cap frame rate at ~60 FPS
+    }
+
+    // Clean up and quit SDL
     SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
     SDL_Quit();
-}
-
-int main(int argc, char *args[])
-{
-    game_is_running = window_initialize();
-
-    while (game_is_running)
-    {
-        process_input();
-        update();
-        render();
-    }
-
-    destroy_window();
     return 0;
 }
